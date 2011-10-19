d1d4d88c788c09a6bf6dc02d4d64b22ea0d8067d
Nathan Weizenbaum <nweiz@google.com>
2011-10-19 16:24:50 -0700

    Add more known bugs.

--- dart-mode.el
+++ dart-mode.el
@@ -32,6 +32,10 @@
 ;; * Multiline strings using """ and ''' are not recognized. They fontify
 ;;   correctly, but only because they look like three strings in a row.
 ;; * In a map with identifier keys, the first key is fontified like a label.
+;; * Return values for operator methods aren't fontified correctly.
+;; * String interpolation isn't fontified as Dart.
+;; * Methods and constructors without return types aren't fontified correctly.
+;; * Untyped parameters aren't fontified correctly.
 
 ;;; Code:
 
