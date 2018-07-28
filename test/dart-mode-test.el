6bc08f7232c218e16d7de549de68ca161634de44
Brady Trainor <mail@bradyt.com>
2018-07-28 13:22:39 -0700

    Adds failing test against dart dev
    This test succeeds on stable branch of dart, and fails on development
    branch of dart.
    
    This test is fragile, relying on events that are not completely
    understood by its author. Especially, the process seems to return at
    least twice, and we have to limit the time for the third return.
    
    Another instance of difficulty in testing of asynchronous processes
    can be seen at
    https://lists.gnu.org/archive/html/emacs-devel/2013-02/msg00506.html.

--- test/dart-mode-test.el
+++ test/dart-mode-test.el
@@ -2,4 +2,13 @@
 
 (require 'dart-mode)
 
+(ert-deftest dart-test ()
+  (let ((file (make-temp-file "analysis")))
+    (dart-add-analysis-root-for-file file)
+    (let ((proc (dart--analysis-server-process
+		 dart--analysis-server)))
+      (accept-process-output proc)
+      (accept-process-output proc)
+      (accept-process-output proc 8))))
+
 ;;; dart-mode-test.el ends here
