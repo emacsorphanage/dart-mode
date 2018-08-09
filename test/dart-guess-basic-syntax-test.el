7e1aa89c24a95d4fc7ccc9b6d904aa0e924fecd3
Brady Trainor <mail@bradyt.com>
2018-08-08 17:42:23 -0700

    Refactor syntax tests

--- test/dart-guess-basic-syntax-test.el
+++ test/dart-guess-basic-syntax-test.el
@@ -1,51 +1,56 @@
 
+(defun remove-char (char)
+  (save-excursion
+    (goto-char (point-min))
+    (when (search-forward char nil t)
+      (setq beg (match-beginning 0))
+      (delete-region beg (match-end 0))
+      beg)))
+
+(defun set-buffer-string-with-point (string)
+  (insert string)
+  (let ((anchor (remove-char "↓"))
+        (point (remove-char "·")))
+    (goto-char point)
+    anchor))
+
+(defun ert-dart-syntax-test (type string)
+  (let (dart-enable-analysis-server)
+    (with-temp-buffer
+      (dart-mode)
+      (let* ((anchor (set-buffer-string-with-point string))
+             (syntax (dart-guess-basic-syntax)))
+        (if anchor
+            (should (equal syntax (list (list type anchor))))
+          (should (equal syntax (list (list type)))))))))
+
+(ert-deftest dart-syntax-comment-test ()
+  (ert-dart-syntax-test 'comment "
+/*
+ *·
+ */
+"))
+
 (ert-deftest dart-syntax-block-intro-test ()
-  (with-temp-buffer
-    (insert "
+  (ert-dart-syntax-test 'block-intro "
 ↓void main() {
   ·bool stmt = True;
   bool stmt = True;
 }
-")
-    (goto-char (point-min))
-    (let (anchor point syntax)
-      (setq anchor (1- (search-forward "↓")))
-      (delete-char -1)
-      (setq point (1- (search-forward "·")))
-      (delete-char -1)
-      (should (equal (dart-guess-basic-syntax)
-                     `((block-intro ,anchor)))))))
+"))
 
 (ert-deftest dart-syntax-statement-test ()
-  (with-temp-buffer
-    (insert "
+  (ert-dart-syntax-test 'statement "
 void main() {
   ↓bool stmt = True;
   ·bool stmt = True;
 }
-")
-    (goto-char (point-min))
-    (let (anchor point syntax)
-      (setq anchor (1- (search-forward "↓")))
-      (delete-char -1)
-      (setq point (1- (search-forward "·")))
-      (delete-char -1)
-      (should (equal (dart-guess-basic-syntax)
-                     `((statement ,anchor)))))))
+"))
 
 (ert-deftest dart-syntax-close-test ()
-  (with-temp-buffer
-    (insert "
+  (ert-dart-syntax-test 'close "
 ↓void main() {
   bool stmt = True;
   bool stmt = True;
 ·}
-")
-    (goto-char (point-min))
-    (let (anchor point syntax)
-      (setq anchor (1- (search-forward "↓")))
-      (delete-char -1)
-      (setq point (1- (search-forward "·")))
-      (delete-char -1)
-      (should (equal (dart-guess-basic-syntax)
-                     `((close ,anchor)))))))
+"))
